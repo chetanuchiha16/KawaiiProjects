e6f0fe2f3031b95ce8d407d00ea026b5b354b267
chetanuchiha16 <chetan16ck@gmail.com>
2025-05-15 04:33:01 +0530

    Update topsort.c

--- C/4th_Sem_ADA/topsort.c
+++ C/4th_Sem_ADA/topsort.c
@@ -1,67 +1,73 @@
-	#include<stdio.h>
-	void read_matrix(int n,int a[10][10])
-	{
-		int i,j;
-		for(i=0;i<n;i++)
-		{
-			for(j=0;j<n;j++)
-			{
-				scanf("%d",&a[i][j]);
-			}
-		}
-	}
-	void find_indegree(int n,int a[10][10],int indegree[])
-	{
-		int i,j,sum;
-		for(j=0;j<n;j++)
-		{
-			sum=0;
-			for(i=0;i<n;i++)
-			sum+=a[i][j];
-			indegree[j]=sum;
-		}
-	}
-	void topo(int n,int a[10][10])
-	{
-		int i,s[10],indegree[10],k,u,v,t[10],top;
-		find_indegree(n,a,indegree);
-		top=-1;
-		k=0;
-		for(i=0;i<n;i++)
-		{
-			if(indegree[i]==0)
-			s[++top]=i;
-		}
-		while(top!=-1)
-		{
-			u=s[top--];
-			t[k++]=u;
-			for(v=0;v<n;v++)
-			{
-				if(a[u][v]==1)
-				{
-					indegree[v]--;
-					if(indegree[v]==0)
-					s[++top]=v;
-				}
-			}
-		}
-		if(k!=n)
-		printf("\nTopological Sequence not possible\n");
-		else
-		{
-			printf("\nThe Topological Sequence\n");
-			for(i=0;i<n;i++)
-			printf("%d\t",t[i]);
-		}
-	}
-	int main()
-	{
-		int n,a[10][10];
-		printf("enter number of nodes\n");
-		scanf("%d",&n);
-		printf("Enter adjacency matrix\n");
-		read_matrix(n,a);
-		topo(n,a);
-		return 0;
-	}
+#include <stdio.h>
+
+void read_matrix(int nodes, int adj[10][10]) {
+    int row, col;
+    for(row = 0; row < nodes; row++) {
+        for(col = 0; col < nodes; col++) {
+            scanf("%d", &adj[row][col]);
+        }
+    }
+}
+
+void calculate_indegree(int nodes, int adj[10][10], int indegree[]) {
+    int row, col, total;
+    for(col = 0; col < nodes; col++) {
+        total = 0;
+        for(row = 0; row < nodes; row++) {
+            total += adj[row][col];
+        }
+        indegree[col] = total;
+    }
+}
+
+void topological_sort(int nodes, int adj[10][10]) {
+    int i, stack[10], indegree[10], top = -1;
+    int result[10], result_index = 0;
+    int current_node, neighbor;
+
+    calculate_indegree(nodes, adj, indegree);
+
+    for(i = 0; i < nodes; i++) {
+        if(indegree[i] == 0) {
+            stack[++top] = i;
+        }
+    }
+
+    while(top != -1) {
+        current_node = stack[top--];
+        result[result_index++] = current_node;
+
+        for(neighbor = 0; neighbor < nodes; neighbor++) {
+            if(adj[current_node][neighbor] == 1) {
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0) {
+                    stack[++top] = neighbor;
+                }
+            }
+        }
+    }
+
+    if(result_index != nodes) {
+        printf("\nTopological Sequence not possible\n");
+    } else {
+        printf("\nThe Topological Sequence:\n");
+        for(i = 0; i < nodes; i++) {
+            printf("%d\t", result[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int nodes, adj_matrix[10][10];
+
+    printf("Enter number of nodes:\n");
+    scanf("%d", &nodes);
+
+    printf("Enter adjacency matrix:\n");
+    read_matrix(nodes, adj_matrix);
+
+    topological_sort(nodes, adj_matrix);
+
+    return 0;
+}
