b7c821f95111b4c128c50123302e639509868438
chetanuchiha16 <chetan16ck@gmail.com>
2025-05-23 12:25:16 +0530

    Refactor code structure for improved readability and maintainability

--- C/4th_Sem_ADA/subset.c
+++ C/4th_Sem_ADA/subset.c
@@ -1,51 +1,60 @@
-#include <stdio.h>
+#include<stdio.h>
 #define MAX 10
 
-void sumOfSubsets(int set[], int include[], int targetSum, int numElements,
-                  int currSum, int currIndex, int remainingSum) {
-  include[currIndex] = 1;
+void findSubsets(int set[], int include[], int currentSum, int currentIndex, int remainingSum, int targetSum, int totalElements)
+{
+    include[currentIndex] = 1;
 
-  if ((currSum + set[currIndex]) == targetSum) {
-    printf("{\t");
-    for (int i = 1; i <= currIndex; i++)
-      if (include[i] == 1) printf("%d\t", set[i]);
-    printf("}\n");
-  } else if ((currSum + set[currIndex] + set[currIndex + 1]) <= targetSum) {
-    sumOfSubsets(set, include, targetSum, numElements, currSum + set[currIndex],
-                 currIndex + 1, remainingSum - set[currIndex]);
-  }
+    if ((currentSum + set[currentIndex]) == targetSum)
+    {
+        printf("{\t");
+        for (int i = 0; i <= currentIndex; i++)
+        {
+            if (include[i] == 1)
+                printf("%d\t", set[i]);
+        }
+        printf("}\n");
+    }
+    else if ((currentIndex + 1 < totalElements) && (currentSum + set[currentIndex] + set[currentIndex + 1]) <= targetSum)
+    {
+        findSubsets(set, include, currentSum + set[currentIndex], currentIndex + 1, remainingSum - set[currentIndex], targetSum, totalElements);
+    }
 
-  if ((currSum + remainingSum - set[currIndex] >= targetSum) &&
-      (currSum + set[currIndex + 1] <= targetSum)) {
-    include[currIndex] = 0;
-    sumOfSubsets(set, include, targetSum, numElements, currSum, currIndex + 1,
-                 remainingSum - set[currIndex]);
-  }
+    if ((currentSum + remainingSum - set[currentIndex] >= targetSum) &&
+        (currentIndex + 1 < totalElements && currentSum + set[currentIndex + 1] <= targetSum))
+    {
+        include[currentIndex] = 0;
+        findSubsets(set, include, currentSum, currentIndex + 1, remainingSum - set[currentIndex], targetSum, totalElements);
+    }
 }
 
-int main() {
-  int set[MAX], include[MAX];
-  int numElements, targetSum, totalSum = 0;
+int main()
+{
+    int set[MAX], include[MAX];
+    int totalElements, targetSum;
+    int totalSum = 0;
 
-  printf("Enter the number of elements in the set:\n");
-  scanf("%d", &numElements);
+    printf("Enter the number of elements in the set:\n");
+    scanf("%d", &totalElements);
 
-  printf("Enter the set elements in increasing order:\n");
-  for (int i = 1; i <= numElements; i++) {
-    scanf("%d", &set[i]);
-  }
+    printf("Enter the set elements in increasing order:\n");
+    for (int i = 0; i < totalElements; i++)
+    {
+        scanf("%d", &set[i]);
+        totalSum += set[i];
+    }
 
-  printf("Enter the sum value:\n");
-  scanf("%d", &targetSum);
+    printf("Enter the target sum:\n");
+    scanf("%d", &targetSum);
 
-  for (int i = 1; i <= numElements; i++) {
-    totalSum += set[i];
-  }
+    if (totalSum < targetSum)
+    {
+        printf("\nNo subset possible\n");
+    }
+    else
+    {
+        findSubsets(set, include, 0, 0, totalSum, targetSum, totalElements);
+    }
 
-  if (totalSum < targetSum) {
-    printf("\nNo subset possible\n");
-  } else {
-    sumOfSubsets(set, include, targetSum, numElements, 0, 1, totalSum);
-  }
-  return 0;
+    return 0;
 }
